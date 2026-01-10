f2a2af019af8da6d20853bdb11e0b3f0f4bc163d
apps <apps@agapetransportation.com>
2026-01-10 14:40:34 +0000

    File changes

--- src/components/service/generateAnnouncementsPDF.jsx
+++ src/components/service/generateAnnouncementsPDF.jsx
@@ -5,7 +5,7 @@
 
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+pdfMake.vfs = pdfFonts.vfs;
 
 export function generateAnnouncementsPDF(announcements, serviceDate) {
   // Split announcements
--- src/components/service/generateProgramPDF.jsx
+++ src/components/service/generateProgramPDF.jsx
@@ -6,7 +6,7 @@
 
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+pdfMake.vfs = pdfFonts.vfs;
 
 export function generateServiceProgramPDF(serviceData) {
   const docDefinition = {
