e3f28a9881f69860fd38277b40de713edd5455ea
apps <apps@agapetransportation.com>
2026-02-05 21:37:37 +0000

    File changes

--- functions/SundayMessage.ts
+++
@@ -1,7 +0,0 @@
-// Redirect shortcut for the weekly service submission form.
-// Shareable URL: /api/functions/SundayMessage
-// Redirects to: /api/functions/serveWeeklyServiceSubmission
-Deno.serve((req) => {
-    const targetUrl = new URL('/api/functions/serveWeeklyServiceSubmission', req.url);
-    return Response.redirect(targetUrl.toString(), 302);
-});
